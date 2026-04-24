972efe2af0a5e8e4565ce2ccbc355a47150876e6
KingBen-j <coulibalyisrael39@gmail.com>
2026-04-24 00:34:10 +0000

    rendre le formulaire fonctionnel

--- ben_portfolio/views.py
+++ ben_portfolio/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
+from django.http import JsonResponse
+import json
 
 def index(request):
     return render(request, "index.html")
+
+
